6d77284ed156d5c10771cf5042b4ee0acc1a57e6
Otto Schreibke <oschreibke@gmail.com>
2020-03-21 14:08:24 +0100

    SML: Signal Follower event handler

--- Signals_SML.jmri/SignalFollower.py
+++ Signals_SML.jmri/SignalFollower.py
@@ -2,35 +2,48 @@ import jmri
 import java
 import java.beans
 
-#for sn in masts.getNamedBeanSet():
-#   if sn.userName[0] == "v":
-#        print sn.userName + " " + sn.getAspect() # + " " + sn.getAspectName()
+# reflect the aspects shown on basic signal masts onto SBB type L masts
 
-#for m in masts.getNamedBeanSet():
-#    print m.userName + " " + m.getMastType() + " " + m.getSignalSystem() 
+# based on a script by Bob Jacobsen, copyright 2016
+# reworked and extended Otto Schreibke 2020
+# License: GPL
 
-#for m in masts.getNamedBeanSet():
-#    if m.userName[0] != "v":
-#        m.setAspect("Halt")
-
+# map the basic aspects to the SBB aspects
 myDict = {
-"Stop": "Halt",
-"Approach": "FB2",
-"Clear": "FB1"
+    "Stop": "Halt",
+    "Approach": "FB2",
+    "Clear": "FB1"
 }
 
+
+# Define the listeners. 
+class SignalFollowerListener(java.beans.PropertyChangeListener):
+
+  def set(self, oSource, oTarget) :
+    self.oSource = oSource  
+    self.oTarget = oTarget
+    self.oSource.addPropertyChangeListener(self)
+    return
+    
+  def propertyChange(self, event):
+    # map the source aspect to the target and set accordingly
+    self.oTarget.setAspect(myDict.get(self.oSource.getAspect()))
+    return
+
+# Iterate ovet the masts collection
+#   - set the initial state (Halt) of the non-virtual masts
+#     for whatever good reason, simply setting the aspect to be followed doesn't work
+#   - set the event listerers on the source masts
+#   - set the initial follower state
+
 for m in masts.getNamedBeanSet():
-    if m.userName[0] != "v":
-        a = m.getAspect()
-        if not a:
-            a = "Unknown"
-        print m.userName + " " + a 
+    if m.userName[0] != "v":     # not a virtual mast
+        m.setAspect("Halt")      # initialise all followers to "Halt"
+        
+        # set the event handlers and set the initial following
         source = masts.getSignalMast("v" + m.userName)
         if source:
-            print "Source: " + source.userName + " is showing " + source.getAspect() 
-            print "Setting aspect on " + m.userName + " from " + a + " to " + myDict.get(source.getAspect())
+            SignalFollowerListener().set(source, m)
             m.setAspect(myDict.get(source.getAspect()))
-            print m.userName + " " + m.getAspect() 
         else:
-            print "Couldn't find source for " + m.userName
-        
+            print "Couldn't find corresponding source mast for " + m.userName        
